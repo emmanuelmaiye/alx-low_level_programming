49d58a4e4554ad62a09cc7e316e73b7d863494ea
emmanuelmaiye <emmanuel.olorunmaiye@yahoo.com>
2023-06-22 17:38:07 -0700

    updated

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -12,5 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return (0);
+i	return (0);
 }
